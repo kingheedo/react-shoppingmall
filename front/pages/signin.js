36c391651668f886b3f40aacd848132a0181cd6f
heedo-wang <dhkdgmleh@gmail.com>
2021-11-05 23:51:38 +0900

    로그인 화면UI 구현

--- front/pages/signin.js
+++ front/pages/signin.js
@@ -1,11 +1,104 @@
-import React from 'react'
+import React, { useCallback, } from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+import Link from 'next/link';
+import Router from 'next/router';
+import useInput from '../hooks/useInput';
 
-const signin = () => {
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items:center;
+  justify-content: center;
+  background: linear-gradient(
+    rgba(255,255,255,0.3),
+    rgba(255,255,255,0.3)
+  ), url("http://app-storage-edge-003.cafe24.com/bannermanage2/insilence1/2021/11/04/59b551c37c5dcbf7548f4bc8b3386b1d.jpg");
+  background-size: cover;
+                
+`
+const Wrapper = styled.div`
+    width: 20%;
+    padding: 20px;
+    background-color: white;
+`
+const SignUpButton = styled(Button)`
+ float: right;
+`
+const Signin = () => {
+    const [email, onChangeEmail] = useInput('')
+    const [password, onChangePassword] = useInput('')
+
+    const onhandleSubmit = useCallback(
+    (e) => {
+        e.preventDefault();
+        Router.push('/')
+    },
+    [],
+)
+    const onhandleSignUp = useCallback(
+    (e) => {
+        e.preventDefault();
+        Router.push('/signup')
+    },
+    [],
+)
+
     return (
-        <div>
-            로그인
-        </div>
+        <Container>
+            <Wrapper>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onhandleSubmit}
+                    >
+                        <h1>LOGIN</h1>
+                    <Form.Item
+                        name="E-mail"
+                        rules={[
+                        {
+                            required: true,
+                            message: '이메일을 입력해주세요!',
+                        },
+                        ]}
+                    >
+                        <Input value={email} onChange={onChangeEmail} prefix={<UserOutlined className="site-form-item-icon" />} placeholder="E-mail" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                        {
+                            required: true,
+                            message: '비밀번호를 입력해주세요!',
+                        },
+                        ]}
+                    >
+                        <Input
+                        value={password}
+                        onChange={onChangePassword}
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        type="password"
+                        placeholder="Password"
+                        />
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button onClick={onhandleSubmit} type="primary" htmlType="submit" className="login-form-button">
+                        로그인
+                        </Button>
+                        <SignUpButton onClick={onhandleSignUp} type="primary" htmlType="submit" className="signup-form-button">
+                            회원가입
+                        </SignUpButton>
+                    </Form.Item>
+                    </Form>
+            </Wrapper>
+        </Container>
     )
 }
 
-export default signin
+export default Signin
