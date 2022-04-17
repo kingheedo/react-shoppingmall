0bf532143e39b5242418083d665e218d426d5d74
heedo-wang <dhkdgmleh@gmail.com>
2022-04-18 03:56:32 +0900

    Update: config.ts => config.js for ubuntu db create

--- back/config/config.js
+++ back/config/config.js
@@ -1,38 +1,26 @@
-import * as dotenv from "dotenv";
+const dotenv = require('dotenv');
 dotenv.config();
-type Config = {
-  username: string,
-  password: string,
-  database: string,
-  host: string,
-  [key:string]: string,
-}
-interface ConfigGroup{
- development: Config;
- test: Config;
- production: Config;
-}
- const config: ConfigGroup= {
+
+ module.exports = {
   "development": { //개발용
     "username": "root",
-    "password": process.env.DB_PASSWORD!,
+    "password": process.env.DB_PASSWORD,
     "database": "react-shoppingmall",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": { //테스트용
     "username": "root",
-    "password": process.env.DB_PASSWORD!,
+    "password": process.env.DB_PASSWORD,
     "database": "react-shoppingmall",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": { //배포용
     "username": "root",
-    "password": process.env.DB_PASSWORD!,
+    "password": process.env.DB_PASSWORD,
     "database": "react-shoppingmall",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
 }
-export default config
