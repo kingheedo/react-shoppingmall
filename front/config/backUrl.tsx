983f7792608f509ce2290c0c7b2247b0dc552735
heedo-wang <dhkdgmleh@gmail.com>
2022-04-18 18:51:00 +0900

    Update : backUrl

--- front/config/backUrl.tsx
+++ front/config/backUrl.tsx
@@ -1,1 +1,1 @@
-export const backUrl = 'http://api.reactshoppingmall.com';
+export const backUrl = (process.env.NODE_ENV === 'production') ? 'http//api.reactshoppingmall.com' : 'http://localhost:3065';
