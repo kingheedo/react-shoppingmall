5d88e439d9083d42e954f3fa5f59843bce794517
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-03-14 13:57:47 +0900

    fix(포트): 프론트 변경

--- back/app.js
+++ back/app.js
@@ -35,14 +35,14 @@ if (prod) {
     app.use(hpp());
     app.use((0, helmet_1.default)());
     app.use(cors({
-        origin: 'http://13.125.224.64',
+        origin: 'http://52.78.109.197',
         credentials: true,
     }));
 }
 else {
     app.use(morgan('dev'));
     app.use(cors({
-        origin: ['http://localhost:3060', 'http://13.125.224.64'],
+        origin: ['http://localhost:3060', 'http://52.78.109.197'],
         credentials: true,
     }));
 }
