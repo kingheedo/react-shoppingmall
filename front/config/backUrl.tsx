9cab1b4b2b592f788e71a33d3f138c3868cca88b
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-03-14 21:43:40 +0900

    fix(포트): 구입한 도메인으로 변경

--- front/config/backUrl.tsx
+++ front/config/backUrl.tsx
@@ -1,2 +1,2 @@
 // export const backUrl = (process.env.NODE_ENV === 'production') ? 'https//api.reactshoppingmall.com' : 'http://localhost:3065';
-export const backUrl = (process.env.NODE_ENV === 'production') ? 'http://13.125.102.197' : 'http://localhost:3065';
+export const backUrl = (process.env.NODE_ENV === 'production') ? 'http://52.78.120.250' : 'http://localhost:3065';
