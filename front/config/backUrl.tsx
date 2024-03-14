2a06c515bf91738a409f7e18b2ba2ab8084f2352
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-03-14 13:03:33 +0900

    fix(포트): 백엔드 포트 ec2로 변경

--- front/config/backUrl.tsx
+++ front/config/backUrl.tsx
@@ -1,2 +1,2 @@
 // export const backUrl = (process.env.NODE_ENV === 'production') ? 'https//api.reactshoppingmall.com' : 'http://localhost:3065';
-export const backUrl = (process.env.NODE_ENV === 'production') ? 'https://react-shoppingmall-back.vercel.app/' : 'http://localhost:3065';
+export const backUrl = (process.env.NODE_ENV === 'production') ? '3.34.126.160' : 'http://localhost:3065';
