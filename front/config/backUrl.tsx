cf5f9a123f15fe6ab8a7031450eff939f8eb7daa
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-03-14 13:22:43 +0900

    fix(포트): 백엔드 포트 수정

--- front/config/backUrl.tsx
+++ front/config/backUrl.tsx
@@ -1,2 +1,2 @@
 // export const backUrl = (process.env.NODE_ENV === 'production') ? 'https//api.reactshoppingmall.com' : 'http://localhost:3065';
-export const backUrl = (process.env.NODE_ENV === 'production') ? '3.34.126.160' : 'http://localhost:3065';
+export const backUrl = (process.env.NODE_ENV === 'production') ? 'http://3.34.126.160' : 'http://localhost:3065';
